eb3822d366ce7395a3fe86cf554351ccfc4ecc50
Timothée LLORET <timlltjob12@gmail.com>
2026-09-21 13:38:14 +0200

    Phase 2 : premier test "Hello world !"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,14 @@
+<? 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class AccueilController extends AbstractController {
+
+     #[Route('/', name: 'accueil')]
+     public function index(): Response {
+        return new Response("Hello world !");
+   }
+}

e5f0a49b87fe1df632a49609e568bd13410d5866
Timothée LLORET <timlltjob12@gmail.com>
2026-09-21 14:37:29 +0200

    Phase 4 : Plusieurs pages

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class VoyagesController extends AbstractController
 {
-    #[Route('/', name: 'accueil')]
+    #[Route('/voyages', name: 'voyages')]
     public function index(): Response
     {
-        return $this->render('pages/accueil.html.twig');
+        return $this->render('pages/voyages.html.twig');
     }
 }
